dde4d5aa3fa51f112e90d620552bdc11341e40e8
ntompkins <12280402+ntompkins@users.noreply.github.com>
2020-10-11 16:51:47 -0500

    Added: Form1, Program.cs
    Added source code files for project.

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace cadabra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openZipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.ScanButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.ResultsDisplay = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.CurrentDirectoryDisplay = new System.Windows.Forms.TextBox();
+            this.FileBrowserDisplay = new System.Windows.Forms.CheckedListBox();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1195, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openZipToolStripMenuItem,
+            this.openDirectoryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openZipToolStripMenuItem
+            // 
+            this.openZipToolStripMenuItem.Name = "openZipToolStripMenuItem";
+            this.openZipToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.openZipToolStripMenuItem.Text = "Open Zip";
+            this.openZipToolStripMenuItem.Click += new System.EventHandler(this.openZipToolStripMenuItem_Click);
+            // 
+            // openDirectoryToolStripMenuItem
+            // 
+            this.openDirectoryToolStripMenuItem.Name = "openDirectoryToolStripMenuItem";
+            this.openDirectoryToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.openDirectoryToolStripMenuItem.Text = "Open Directory";
+            this.openDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openDirectoryToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.04565F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.95435F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CurrentDirectoryDisplay, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.FileBrowserDisplay, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 31);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1161, 561);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 29);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(205, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Settings";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(104, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Flag";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.ScanButton);
+            this.panel2.Location = new System.Drawing.Point(317, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(307, 29);
+            this.panel2.TabIndex = 2;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(226, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Save Report";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(84, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Detailed Report";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // ScanButton
+            // 
+            this.ScanButton.Location = new System.Drawing.Point(3, 3);
+            this.ScanButton.Name = "ScanButton";
+            this.ScanButton.Size = new System.Drawing.Size(75, 23);
+            this.ScanButton.TabIndex = 0;
+            this.ScanButton.Text = "Scan";
+            this.ScanButton.UseVisualStyleBackColor = true;
+            this.ScanButton.Click += new System.EventHandler(this.ScanButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.ResultsDisplay);
+            this.flowLayoutPanel1.Controls.Add(this.richTextBox1);
+            this.flowLayoutPanel1.Controls.Add(this.maskedTextBox1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(317, 38);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(841, 335);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // ResultsDisplay
+            // 
+            this.ResultsDisplay.Location = new System.Drawing.Point(3, 3);
+            this.ResultsDisplay.Name = "ResultsDisplay";
+            this.ResultsDisplay.Size = new System.Drawing.Size(838, 332);
+            this.ResultsDisplay.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 341);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(842, 160);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(3, 507);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(8, 22);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // CurrentDirectoryDisplay
+            // 
+            this.CurrentDirectoryDisplay.Enabled = false;
+            this.CurrentDirectoryDisplay.Location = new System.Drawing.Point(3, 534);
+            this.CurrentDirectoryDisplay.Name = "CurrentDirectoryDisplay";
+            this.CurrentDirectoryDisplay.Size = new System.Drawing.Size(308, 22);
+            this.CurrentDirectoryDisplay.TabIndex = 6;
+            // 
+            // FileBrowserDisplay
+            // 
+            this.FileBrowserDisplay.FormattingEnabled = true;
+            this.FileBrowserDisplay.Location = new System.Drawing.Point(3, 38);
+            this.FileBrowserDisplay.Name = "FileBrowserDisplay";
+            this.FileBrowserDisplay.Size = new System.Drawing.Size(300, 446);
+            this.FileBrowserDisplay.TabIndex = 5;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 596);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "CADABRA";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button ScanButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckedListBox FileBrowserDisplay;
+        private System.Windows.Forms.TreeView ResultsDisplay;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox CurrentDirectoryDisplay;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.ToolStripMenuItem openZipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDirectoryToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml.Linq;
+
+namespace cadabra
+{
+    public partial class Form1 : Form
+    {
+        // TODO: Don't do this
+        private string[] workingDirectoryFiles = new string[] { };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CurrentDirectoryDisplay_update(object path)
+        {
+            CurrentDirectoryDisplay.Text = path.ToString();
+        }
+
+        // Click event to open a directory
+        private void openDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+
+                    // Store full file paths
+                    workingDirectoryFiles = Directory.GetFiles(fbd.SelectedPath);
+
+                    // Update display with dir contents
+                    string[] fileNames = GetFileNames(fbd.SelectedPath); 
+
+                    // Update window contents
+                    FileBrowserDisplay_update(fileNames);
+
+                    // Update CWD
+                    CurrentDirectoryDisplay_update(fbd.SelectedPath);
+                }
+            }
+        }
+
+        // Click event to unzip a zip file
+        private void openZipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                // Get input zip file
+                DialogResult result = ofd.ShowDialog();
+
+                // Verify file exists
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ofd.FileName))
+                {
+                    // Verify file is a zip
+                    if (Path.GetExtension(ofd.FileName).ToLower() == @".zip")
+                    {
+                        // Replace file name with directory name
+                        string unzipLocation = ofd.FileName.Replace(ofd.SafeFileName, Path.GetFileNameWithoutExtension(ofd.FileName));
+
+                        // Extract file
+                        try
+                        {
+                            ZipFile.ExtractToDirectory(ofd.FileName, unzipLocation);
+                        } catch (System.IO.IOException err)
+                        {
+                            System.Windows.Forms.MessageBox.Show("Error, unzip directory already exists.");
+                            Console.WriteLine(err.ToString());
+                        }
+
+                        // Store full file paths
+                        workingDirectoryFiles = Directory.GetFiles(unzipLocation);
+
+                        // Update file browser with just filenames
+                        string[] fileNames = GetFileNames(unzipLocation);
+                        // Update window contents
+                        FileBrowserDisplay_update(fileNames);
+                        // Update CWD
+                        CurrentDirectoryDisplay_update(unzipLocation);
+
+                    } else
+                    {
+                        System.Windows.Forms.MessageBox.Show("Error, file is not a zip.");
+                    }
+                }
+            }
+        }
+
+        private void FileBrowserDisplay_update(string[] fileList)
+        {
+            // Always start from scratch
+            FileBrowserDisplay.Items.Clear();
+
+            // Update listbox with contents of array
+            for (int i = 0; i < fileList.Length; i++)
+            {
+                FileBrowserDisplay.Items.Add(fileList[i]);
+            }
+        }
+
+        // STANDALONE METHODS
+        private string[] GetFileNames(string curDir)//, string[] tempList)
+        {
+            // Always clear temp list
+            // TODO: Add regex to filter only solidwork files and update method's name
+            return Directory.GetFiles(curDir).Select(Path.GetFileName).ToArray();
+        }
+
+        private string CalculateSHA(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (var crypto = new SHA1CryptoServiceProvider())
+            {
+                return BitConverter.ToString(crypto.ComputeHash(bs));
+            }
+        }
+
+        private bool QuickVerifySHAS(string[] SHAs)
+        {
+            return SHAs.Distinct().Count() < SHAs.Length;
+        }
+
+        private void ScanButton_Click(object sender, EventArgs e)
+        {
+            // List to store SHA sums
+            string[] SHAList = new string[workingDirectoryFiles.Length];
+
+            // Calculate SHA for each file in curdir
+            for (int i = 0; i < workingDirectoryFiles.Length; i++) 
+            {
+                SHAList[i] = CalculateSHA(workingDirectoryFiles[i]);
+            }
+
+            // Output results of scan
+            if (QuickVerifySHAS(SHAList))
+            {
+                System.Windows.Forms.MessageBox.Show("Uh OH! Someone cheated.");
+            } else
+            {
+                System.Windows.Forms.MessageBox.Show("Yippee, they got away with it.");
+            }
+            
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
